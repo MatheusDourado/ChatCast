c7f69052e97aaceb8dd1f1ee654f2374fb85dc6b
Matheus Dourado Viana <30351672+MatheusDourado@users.noreply.github.com>
2023-10-09 19:20:42 -0300

    Update openai.js method OPTIONS

--- api/openai.js
+++ api/openai.js
@@ -6,6 +6,10 @@ module.exports = async (req, res) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
+    
     try {
         const response = await fetch('https://api.openai.com/v1/chat/completions', {
             method: "POST",
