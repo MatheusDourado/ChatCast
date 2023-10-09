2372a8f182c7c6864a56a03632a54b52b87a1d05
Matheus Dourado Viana <30351672+MatheusDourado@users.noreply.github.com>
2023-10-09 18:58:16 -0300

    Update openai.js

--- api/openai.js
+++ api/openai.js
@@ -1,33 +1,35 @@
 const fetch = require('node-fetch');
 
 module.exports = async (req, res) => {
+    // Adicione os cabeçalhos para habilitar CORS
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
     try {
         const response = await fetch('https://api.openai.com/v1/chat/completions', {
             method: "POST",
             headers: {
                 Accept: "application/json",
                 "Content-Type": "application/json",
-                Authorization: "Bearer ${process.env.OPENAI_API_KEY}"
+                Authorization: `Bearer ${process.env.OPENAI_API_KEY}` 
             },
-            body: JSON.stringify({
-                model: "gpt-4",
-                messages: messages,
-                max_tokens: 2048,
-                temperature: 0.5
-            })
+            body: JSON.stringify(req.body)
         });
 
         const data = await response.json();
         
         if (data && data.choices && data.choices.length > 0) {
-            return data.choices[0].message.content.trim();
+            res.status(200).json({ message: data.choices[0].message.content.trim() });
+            return;
         }
-        
-        return "Desculpe, não consegui gerar uma resposta.";
+
+        res.status(200).json({ message: "Desculpe, não consegui gerar uma resposta." });
     } catch (error) {
         if (error.response && error.response.status === 429) {
-            return "Estou recebendo muitas solicitações no momento. Por favor, tente novamente mais tarde.";
+            res.status(429).json({ error: "Estou recebendo muitas solicitações no momento. Por favor, tente novamente mais tarde." });
+            return;
         }
-        throw error;
+        res.status(500).json({ error: "Internal server error", exception: error.message });
     }
 };
