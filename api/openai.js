4af560de008d8b1b233dd610f06780b148c02b40
Matheus Dourado Viana <30351672+MatheusDourado@users.noreply.github.com>
2023-10-09 18:46:00 -0300

    Update openai.js

--- api/openai.js
+++ api/openai.js
@@ -2,17 +2,32 @@ const fetch = require('node-fetch');
 
 module.exports = async (req, res) => {
     try {
-        const response = await fetch('https://api.openai.com/v1/engines/davinci/completions', {
-            method: 'POST',
+        const response = await fetch('https://api.openai.com/v1/chat/completions', {
+            method: "POST",
             headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
+                Accept: "application/json",
+                "Content-Type": "application/json",
+                Authorization: "Bearer ${process.env.OPENAI_API_KEY}"
             },
-            body: JSON.stringify(req.body) 
+            body: JSON.stringify({
+                model: "gpt-4",
+                messages: messages,
+                max_tokens: 2048,
+                temperature: 0.5
+            })
         });
+
         const data = await response.json();
-        res.status(200).json(data);
+        
+        if (data && data.choices && data.choices.length > 0) {
+            return data.choices[0].message.content.trim();
+        }
+        
+        return "Desculpe, não consegui gerar uma resposta.";
     } catch (error) {
-        res.status(500).json({ error: "Internal server error" });
+        if (error.response && error.response.status === 429) {
+            return "Estou recebendo muitas solicitações no momento. Por favor, tente novamente mais tarde.";
+        }
+        throw error;
     }
 };
